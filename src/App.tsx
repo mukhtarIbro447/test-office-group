428ef53a7cde477617d023a1ccfae73e5826f300
Muko Ibrahim <porg>
2023-10-08 12:38:20 +0100

    Added MUI theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import './App.css';
+import { CssBaseline } from '@mui/material';
+import { StyledEngineProvider, createTheme, ThemeProvider as MUIThemeProvider } from '@mui/material/styles';
 import Header from './components/Header';
 import MiddleContent from './components/MiddleContent';
 import WhiteColorFactory from './components/WhiteColorFactory';
 
 function App() {
+  const theme = createTheme();
   return (
-    <div className="App">
-      <Header />
-      <MiddleContent />
-      <WhiteColorFactory />
-    </div>
+    <StyledEngineProvider injectFirst>
+      <MUIThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="App">
+          <Header />
+          <MiddleContent />
+          <WhiteColorFactory />
+        </div>
+      </MUIThemeProvider>
+    </StyledEngineProvider>
   );
 }
 
